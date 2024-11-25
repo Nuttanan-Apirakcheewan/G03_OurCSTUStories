dedadd46dba2660fc705322d9a30d87d2f9ab0c3
Nattanan Apirakcheewan <nuttanan.api@dome.tu.ac.th>
2024-11-25 18:20:53 +0700

    Homepage: Make the responsive card for each member

--- script.js
+++ script.js
@@ -7,3 +7,38 @@ document.addEventListener("DOMContentLoaded", () => {
         menu.classList.toggle("active");
     });
 });
+
+let currentIndex = 0;
+const totalCards = 7; 
+const cardWrapper = document.querySelector('.card-wrapper');
+const prevBtn = document.querySelector('#prev-btn');
+const nextBtn = document.querySelector('#next-btn');
+
+
+function updateSliderPosition() {
+  const cardWidth = document.querySelector('.card').offsetWidth + 15;  
+  cardWrapper.style.transform = `translateX(-${currentIndex * cardWidth}px)`;  
+}
+
+
+nextBtn.addEventListener('click', () => {
+  if (currentIndex < totalCards - 1) {
+    currentIndex++;
+    updateSliderPosition();
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSliderPosition();
+  }
+});
+
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 500) {
+    currentIndex = 0;  
+    updateSliderPosition(); 
+  }
+});
