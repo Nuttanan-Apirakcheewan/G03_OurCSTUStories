ccd489b4ed25078d5b06e5cc292784f9c78c22d6
chaun361 <jsonbrody5@gmail.com>
2024-12-01 14:05:58 +0700

    @media max-width:800px > .card-wrapper , script.js > cardWidth | updated gap to 0 for smoother card transition

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const nextBtn = document.querySelector('#next-btn');
   const form = document.querySelector('form');
   const totalCards = () => document.querySelectorAll('.card').length;
-  const commentsBuffer = []; 
+  const commentsBuffer = [];
 
   const profilePictures = [
     'recources/cat.jpg',
@@ -23,10 +23,10 @@ document.addEventListener("DOMContentLoaded", () => {
     'recources/eva_tiger.png'
   ];
 
-  let lastUsedPictureIndex = -1; 
+  let lastUsedPictureIndex = -1;
 
   function updateSliderPosition() {
-    const cardWidth = document.querySelector('.card').offsetWidth + 15;
+    const cardWidth = document.querySelector('.card').offsetWidth + 0;
     cardWrapper.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
   }
 
@@ -65,53 +65,53 @@ document.addEventListener("DOMContentLoaded", () => {
 
   document.querySelectorAll('.guestForm').forEach(form => {
     form.addEventListener('submit', (event) => {
-      event.preventDefault(); 
-  
+      event.preventDefault();
+
       const isDesktopForm = form.id === 'desktopForm';
       const isMobileForm = form.id === 'mobileForm';
-  
-   
+
+
       const name = form.querySelector('#nameTh').value + ' ' + form.querySelector('#surnameTh').value;
       const device = form.querySelector('input[name="userDevice"]:checked').value;
       const comment = form.querySelector('#comment').value;
       const email = form.querySelector('#email').value;
-  
+
       const colorVariables = ['--color1', '--color2', '--color3', '--color4', '--color5'];
-  
+
       let randomIndex;
       do {
         randomIndex = Math.floor(Math.random() * colorVariables.length);
       } while (randomIndex === lastUsedColorIndex);
-  
+
       lastUsedColorIndex = randomIndex;
       const randomColor = colorVariables[randomIndex];
-  
+
       let randomPictureIndex;
       do {
         randomPictureIndex = Math.floor(Math.random() * profilePictures.length);
       } while (randomPictureIndex === lastUsedPictureIndex);
-  
+
       const randomPicture = profilePictures[randomPictureIndex];
-      lastUsedPictureIndex = randomPictureIndex; 
-  
+      lastUsedPictureIndex = randomPictureIndex;
+
       commentsBuffer.push({ name, device, comment });
-  
+
       if (commentsBuffer.length % 3 === 1) {
         const newCard = document.createElement('div');
         newCard.className = 'card';
         cardWrapper.appendChild(newCard);
       }
-      
+
       const latestCard = document.querySelector('.card-wrapper .card:last-child');
       const lastestcomment = document.querySelector('.sidebar');
-  
+
       const userComment = document.createElement('div');
       const userCommentDesktop = document.createElement('div');
-  
+
       userComment.className = 'user-comment';
       userCommentDesktop.className = 'user-comment-desktop';
-     
-      
+
+
       userComment.style.setProperty('--random-color', `var(${randomColor})`);
       userCommentDesktop.style.setProperty('--random-color', `var(${randomColor})`);
       userComment.innerHTML = `
@@ -133,30 +133,30 @@ document.addEventListener("DOMContentLoaded", () => {
       <p><strong>Email:</strong> ${email}</p>
       <p><strong>Comment:</strong> ${comment}</p>
     `;
-  
+
       latestCard.appendChild(userComment);
       lastestcomment.appendChild(userCommentDesktop);
-  
- 
+
+
       if (isMobileForm) {
         currentIndex = totalCards() - 1;
         updateSliderPosition();
       }
-      
+
       form.reset();
     });
   });
 
-document.getElementById("guestbookButton").addEventListener("click", function() {
-  const sidebar = document.getElementById("sidebar");
-  
+  document.getElementById("guestbookButton").addEventListener("click", function () {
+    const sidebar = document.getElementById("sidebar");
 
-  if (sidebar.style.left === "0px") {
-    sidebar.style.left = "-350px";  
-  } else {
-    sidebar.style.left = "0px";  
-  }
-});
+
+    if (sidebar.style.left === "0px") {
+      sidebar.style.left = "-350px";
+    } else {
+      sidebar.style.left = "0px";
+    }
+  });
 });
 
 //----------------------------Journey page-----------------------------
